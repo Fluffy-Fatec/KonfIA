a6f954d30ae64c71b8ce7ebcdd120f265e172ac1
Luiz_Borges <88781739+luizborges17@users.noreply.github.com>
2023-05-28 13:00:30 -0300

    upload project springapplication

---
+++ SpringApplication/konfIA/src/main/java/com/fluffy/konfIA/KonfIaApplication.java
@@ -0,0 +1,13 @@
+package com.fluffy.konfIA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KonfIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KonfIaApplication.class, args);
+	}
+
+}
---
+++ SpringApplication/konfIA/src/test/java/com/fluffy/konfIA/KonfIaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fluffy.konfIA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KonfIaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
